1364f183c78d7072ac837033dfc93356f5d4a628
Matthew J. Morrison <m.james.morrison00@gmail.com>
2016-10-19 12:04:59 -0400

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+    entry: './lib/index.js',                    // The main file for our app
+
+    output: {
+        path: './',                             // The path to write our compiled app to
+        filename: 'bundle.js'                   // The new name for our compiled app
+    },
+    devtool: 'source-map',                      //
+    module: {                                   // Define which transformations to make on our code
+        loaders: [                              // Instruct webpack to run source files through the specified loaders
+            {
+                test: /\.js$/,                  // Define which files match the criteria for being run through loader
+                exclude: /node_modules/,        // Define which files to exclude from being run through loader
+                loaders: [                      // Which loader is going to be used?
+                    'babel-loader'              // 'babel-loader' instructs Webpack to use .babelrc file to define loaders here
+                ]
+            },
+            {
+                test: /\.scss$/,
+                // loaders: [ 'style', 'css?sourceMap', 'sass?sourceMap' ]
+                loader: ExtractTextPlugin.extract(
+                    'style', // The backup style loader
+                    'css?sourceMap!sass?sourceMap'
+                )
+            }
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin('./lib/css/main.css')
+    ]
+};
