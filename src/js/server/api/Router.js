49ae1c436b1f599adace1aa5103658ef7e035249
Matthew J. Morrison <m.james.morrison00@gmail.com>
2016-12-04 02:31:02 -0500

    Registering is good to go. Login was almost there, but is not bugging out about some unspecified view engine. Hrmph.

--- src/js/server/api/Router.js
+++ src/js/server/api/Router.js
@@ -33,6 +33,7 @@ router.post('/register', (req, res, next) => {
             if (err) {
                 return next(err);
             } else {
+                req.session.userId = user._id;
                 return res.redirect('/lists');
             }
         });
@@ -49,13 +50,24 @@ router.post('/register', (req, res, next) => {
 router.post('/login', (req, res, next) => {
     if (req.body.email
         && req.body.password) {
-        console.log("Login success!");
+        User.authenticate(req.body.email, req.body.password, (error, user) => {
+            if (err || !user) {
+                var err = new Error('Wrong email or password');
+                err.status = 401;
+                return next(err);
+            } else {
+                req.session.userId = user._id;
+                return res.redirect('/lists');
+            }
+        });
     } else {
-        console.log("Login error");
+        // console.log("Login error");
         var err = new Error('Email and password are required.');
         err.status = 401;
         return next(err);
     }
+
+    return next();
     
 });
 
--- src/js/server/models/Users.js
+++ src/js/server/models/Users.js
@@ -21,6 +21,30 @@ var UserSchema = new mongoose.Schema({
 
 // Authenticate login input vs. database document
 // 'Statics' lets you add methods directly to the model
+UserSchema.statics.authenticate = (email, password, callback) => {
+    User.findOne({ email: email })
+        .exec((err, user) => {
+            if (err) {
+                return callback(err);
+            } else if (!user){
+                var err = new Error('User not found.');
+                err.status - 401;
+                return callback(err);
+            }
+
+            // At this point, the user exists. So compare the given password to the database password
+            bcrypt.compare(password, user.password, (err, result) => {
+                if (result === true) {
+                    return callback(null, user);
+                } else {
+                    return callback(err);
+                }
+            });
+        });
+}
+
+// Pre-save hook: runs just before saving record to mongo (hence 'save' keyword)
+// Hash password before saving to database
 UserSchema.pre('save', function (next) {
     var user = this;
 
@@ -34,10 +58,6 @@ UserSchema.pre('save', function (next) {
         next();
     });
 });
-
-// Pre-save hook: runs just before saving record to mongo (hence 'save' keyword)
-// Hash password before saving to database
-
 
 var UserModel = mongoose.model('User', UserSchema);
 
